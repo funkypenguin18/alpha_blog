9396ef3e21d2cd214bbfabff46df717a8201ea49
Олег Данилов <olegdanilov@Olegs-MacBook-Pro.local>
2018-11-24 03:18:17 +0300

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, exept: [:new]
 end
