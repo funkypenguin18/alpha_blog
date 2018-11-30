d4c403d189349784c484404e0ae65e411654a11d
Олег Данилов <olegdanilov@Olegs-MacBook-Pro.local>
2018-11-30 19:31:58 +0300

    Add users listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -33,7 +38,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password, :id)
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
