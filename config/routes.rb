70950b22dec2f7594a9cbca46873eae903432964
Олег Данилов <olegdanilov@Olegs-MacBook-Pro.local>
2018-12-01 14:46:54 +0300

    Complete auth system, update current_user to be article creator, fix gravatar display error

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   #get 'users', to: 'user#edit'
   resources :users, exept: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
